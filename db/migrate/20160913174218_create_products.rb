865f3ad917dd6765f5b327e06c4fc9fe72234793
Erik Young <erik.young@rmit.edu.vn>
2016-09-17 22:09:54 +0700

    3.11 dropped the string

--- db/migrate/20160913174218_create_products.rb
+++ db/migrate/20160913174218_create_products.rb
@@ -2,7 +2,6 @@ class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.string :string
       t.text :description
       t.string :image_url
 
