328f62ff83aacf5a673692e330fbd994192a85c0
erikyoung <erik.young@rmit.edu.vn>
2016-09-29 15:05:32 +0700

    changes to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919154932) do
+ActiveRecord::Schema.define(version: 20160921190956) do
 
   create_table "frees", force: :cascade do |t|
     t.string   "speaking"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160919154932) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "colour"
   end
 
 end
