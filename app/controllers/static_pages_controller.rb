3fdc4deae6b7112ca43e7b17f7cb0d422d28edc3
erikyoung <erik.young@rmit.edu.vn>
2016-10-26 03:22:12 +0700

    adding product page and carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
   def index
+
   end
 
   def landing_page
     @featured_product = Product.limit(3)
+    @products = Product.limit(3)
   end
 end
